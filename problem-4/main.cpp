44fe2df08660e2d17ee952eace6dd836102623d5
sblekic <sblekic1@etf.unsa.ba>
2024-04-22 21:19:51 +0200

    reversed the order of words

--- problem-4/main.cpp
+++ problem-4/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
+
+std::string IzvrniRedoslijedRijeci(const std::string &s);
 
 int main() {
-    std::cout << "solution for problem-4 coming today" << std::endl;
+    std::cout << "Enter the string: ";
+    std::string s;
+    std::getline(std::cin, s);
+    std::cout << "Reversed string: " << IzvrniRedoslijedRijeci(s) << std::endl;
     return 0;
+}
+
+std::string IzvrniRedoslijedRijeci(const std::string &s) {
+    std::string reversed;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] != ' ' && (i == 0 || s[i - 1] == ' ')) { // finding the beggining of the word (first word or nth word)
+            std::string word;
+            while (i < s.length() && s[i] != ' ') word += s[i++];
+            if (reversed == "") reversed = word;
+            else { reversed = word + ' ' + reversed; } // word + space + everything that was before
+        }
+    }
+    return reversed;
 }
