ffa86f3cf6bd95010f5d687741e8222014e2d1cd
sblekic <sblekic1@etf.unsa.ba>
2024-05-02 18:26:52 +0200

    plus3 function implemented

--- problem-8/main.cpp
+++ problem-8/main.cpp
@@ -7,8 +7,27 @@ std::function<double(double)> plus2(double x) {
     };
 }
 
+auto plus3(double x) {
+    return [x](double y) {
+        return [x, y](double z) { return x + y + z; };
+    };
+}
+
 int main() {
+    std::cout << "plus2: " << std::endl;
     auto f = plus2(2);
     std::cout << f(3) << std::endl;
+    std::cout << "plus3: " << std::endl;
+    auto g = plus3(2);
+    std::cout << g(3)(4) << std::endl;
+
+    // or
+    // std::cout << plus2(3)(5) << std::endl;
+    // std::cout << plus3(3)(5)(6) << std::endl;
     return 0;
 }
+
+// note:
+// return type for plus3 is function<function<double(double)>(double)>
+// line 11: return type is function<double(double)>
+// line 13: return type is double
