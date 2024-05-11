aa02ef6af414fd2f5e0e6163bb42c7581bdfc6e1
sblekic <sblekic1@etf.unsa.ba>
2024-05-11 23:23:35 +0200

    used std::rotate on std::list<std::list<double>>, p13

--- problem-13/main.cpp
+++ problem-13/main.cpp
@@ -1,6 +1,66 @@
 #include <iostream>
+#include <list>
+#include <queue>
+#include <algorithm>
+#include <iomanip>
+
+std::queue<bool> RotirajKoloneMatrice(std::list<std::list<double>> &matrica) {
+    std::queue<bool> result_queue;
+    auto first_row_size = matrica.begin()->size();
+    for(auto &row : matrica) {
+        if(row.size() != first_row_size)
+            throw std::domain_error("Parametar nema formu matrice!");
+        double p = 1;
+        for(double e : row) p *= e; // proizvod elemenata tekuceg reda matrice
+        result_queue.push(p > 0); // ako je proizvod pozitivan, dodaje se true u red
+        auto start = row.begin();
+        std::rotate(row.begin(), ++start, row.end());
+    }
+    return result_queue;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Unesite broj redova: ";
+    int m;
+    std::cin >> m;
+    std::cout << "Unesite broj kolona: ";
+    int n;
+    std::cin >> n;
+    std::list<std::list<double>> matrica(m, std::list<double>(n)); // matrica dimenzija m x n
+    std::cout << "Unesite elemente matrice: ";
+    for(auto &row : matrica)
+        for(double &el : row)
+            std::cin >> el;
+
+    std::queue<bool> result_q = RotirajKoloneMatrice(matrica);
+
+    // print matrix
+    std::cout << "Transformirana matrica:" << std::endl;
+    for(auto &red : matrica) {
+        for(double &e : red) std::cout << std::setw(8) << e;
+        std::cout << std::endl;
+    }
+
+    // print result queue
+    std::cout << "Rezultirajuci red: ";
+    while(!result_q.empty()) {
+        std::cout << std::boolalpha << result_q.front() << " ";
+        result_q.pop();
+    }
     return 0;
 }
+
+
+// 5x3
+// 1         2      -3
+// 1.5     2.5     3.5
+// 4         5       6
+// 5.5    -3.5    -5.5
+// 9         8       7
+
+// nakon rotacije
+//    2      -3     1
+//  2.5     3.5   1.5
+//    5       6     4
+// -3.5    -5.5   5.5
+//    8       7     9
