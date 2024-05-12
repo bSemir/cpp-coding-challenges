79c54fb7f91c183f3d2e2db9f11e5187dee8e049
sblekic <sblekic1@etf.unsa.ba>
2024-05-13 00:14:21 +0200

    class Stedni Racun improved, p14

--- problem-14/main.cpp
+++ problem-14/main.cpp
@@ -4,35 +4,31 @@
 
 class StedniRacun {
 private:
-    StedniRacun(double stanje_racuna = 0);
     double stanje_racuna;
 public:
-    StedniRacun Ulozi(double iznos);
-    StedniRacun Podigni(double iznos);
-    StedniRacun ObracunajKamatu(double kamata);
+    // constructor with initialization list
+    StedniRacun(double stanje_racuna = 0) : stanje_racuna(stanje_racuna) {
+        if(stanje_racuna < 0) throw std::logic_error("Nedozvoljeno pocetno stanje");
+    }
+    StedniRacun &Ulozi(double iznos);
+    StedniRacun &Podigni(double iznos);
+    StedniRacun &ObracunajKamatu(double kamata);
     double DajStanje() const { return stanje_racuna; }
 };
 
-// define constructor
-StedniRacun::StedniRacun(double stanje_racuna) {
-    // konstruktor treba omoguciti automatsku konverziju realnih brojeva u objekte tipa StedniRacun
-    if(stanje_racuna < 0) throw std::logic_error("Nedozvoljeno pocetno stanje");
-    this->stanje_racuna = stanje_racuna;
-}
-
-StedniRacun StedniRacun::Ulozi(double iznos) {
+StedniRacun &StedniRacun::Ulozi(double iznos) {
     if(iznos < 0) throw std::logic_error("Nedozvoljena transakcija(ulog je negativan)");
     stanje_racuna += iznos;
     return *this;
 }
 
-StedniRacun StedniRacun::Podigni(double iznos) {
-    if(iznos <= stanje_racuna) stanje_racuna -= iznos;
-    else throw std::logic_error("Transakcija odbijena");
+StedniRacun &StedniRacun::Podigni(double iznos) {
+    if(iznos > stanje_racuna) throw std::logic_error("Transakcija odbijena");
+    stanje_racuna -= iznos;
     return *this;
 }
 
-StedniRacun StedniRacun::ObracunajKamatu(double kamata){
+StedniRacun &StedniRacun::ObracunajKamatu(double kamata){
     if(kamata < 0) throw std::logic_error("Nedozvoljena kamatna stopa");
     stanje_racuna += stanje_racuna * kamata / 100; // / 100 jer je kamata u procentima
     return *this;
