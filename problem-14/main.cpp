d54792e385e2048c69dce5d38c307937c53c0d55
sblekic <sblekic1@etf.unsa.ba>
2024-05-13 19:10:18 +0200

    finished and tested class Stedni Racun

--- problem-14/main.cpp
+++ problem-14/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <stdexcept>
 
-
 class StedniRacun {
 private:
     double stanje_racuna;
@@ -17,9 +16,10 @@ public:
 };
 
 StedniRacun &StedniRacun::Ulozi(double iznos) {
-    if(iznos < 0) throw std::logic_error("Nedozvoljena transakcija(ulog je negativan)");
-    stanje_racuna += iznos;
-    return *this;
+//    if(iznos < 0) throw std::logic_error("Nedozvoljena transakcija(ulog je negativan)");
+//    stanje_racuna += iznos;
+//    return *this;
+    return Podigni(-iznos);
 }
 
 StedniRacun &StedniRacun::Podigni(double iznos) {
@@ -34,7 +34,46 @@ StedniRacun &StedniRacun::ObracunajKamatu(double kamata){
     return *this;
 }
 
+// generic function to test if proper exception is thrown. Full type deduction used
+template <typename exception_type, typename function_type>
+void testException(function_type action) {
+    try {
+        action();
+    } catch (exception_type &e) {
+        std::cout << e.what() << "\n";
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    StedniRacun s1;
+    StedniRacun s2(100);
+    StedniRacun s3 = 30;
+    std::cout << "(s1, s2, s3): " << s1.DajStanje() << " " << s2.DajStanje() << " " << s3.DajStanje() << "\n"; // 0 100 30
+    s1.Ulozi(30); s2.Ulozi(5); s3.Ulozi(20);
+    std::cout << "(s1, s2, s3): " << s1.DajStanje() << " " << s2.DajStanje() << " " << s3.DajStanje() << "\n"; // 30 105 50
+    s1.Ulozi(20); s2.Podigni(50);
+    std::cout << "(s1, s2): " << s1.DajStanje() << " " << s2.DajStanje() << " " << std::endl; // 50 55
+
+    s1.Ulozi(-20); s2.Podigni(-50);
+    std::cout << "(s1, s2): " << s1.DajStanje() << " " << s2.DajStanje() << " " << std::endl; // 30 105
+
+    s1.ObracunajKamatu(5);
+    std::cout << "Nakon kamate od 5%: " << s1.DajStanje() << std::endl; // 31.5
+
+    s1 = 100;
+    std::cout << "(s1) +50, -20: " << s1.Ulozi(50).Podigni(20).DajStanje() << std::endl; // 130
+
+    std::cout << s1.ObracunajKamatu(5).DajStanje() << "\n"; // 136.5
+
+    std::cout << StedniRacun(80).Ulozi(30).DajStanje() << std::endl; // 110
+
+    const StedniRacun s4 = s3; // s3 is 50 here
+    std::cout << "s4: " << s4.DajStanje() << std::endl; // 50
+
+    testException<std::logic_error>([]() { StedniRacun s5(-30); });
+    testException<std::logic_error>([&]() { s1.Podigni(1000); });
+    testException<std::logic_error>([&]() { s1.ObracunajKamatu(-5); });
+    testException<std::logic_error>([&]() { s1.Ulozi(-1000); });
+
     return 0;
 }
