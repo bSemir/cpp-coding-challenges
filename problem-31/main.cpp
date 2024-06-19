8da889287881a198d1701b051908500bac07893d
sblekic <sblekic1@etf.unsa.ba>
2024-06-19 22:55:46 +0200

    destructor, copy constructor, copy assignment operator, dodajSanduk(), dodajBure()

--- problem-31/main.cpp
+++ problem-31/main.cpp
@@ -8,15 +8,21 @@ private:
     std::string sadrzaj;
 public:
     virtual ~Spremnik() {};
-    Spremnik(const std::string &vrsta, double tezina, const char sadrzaj[]) : vrsta(vrsta), tezina(tezina), sadrzaj(sadrzaj)  {}
+
+    Spremnik(const std::string &vrsta, double tezina, const char sadrzaj[]) : vrsta(vrsta), tezina(tezina),
+                                                                              sadrzaj(sadrzaj) {}
+
     double DajTezinu() const { return tezina; }
+
     virtual double DajUkupnuTezinu() const = 0;
+
     virtual Spremnik *DajKopiju() const = 0;
+
     void Ispisi(std::ostream &izlaz = std::cout) const {
         izlaz << "Vrsta spremnika: " << vrsta << std::endl
-            << "Sadrzaj: " << sadrzaj << std::endl
-            << "Vlastita tezina: " << tezina << " kg" << std::endl
-            << "Ukupna tezina: " << DajUkupnuTezinu() << " kg" << std::endl;
+              << "Sadrzaj: " << sadrzaj << std::endl
+              << "Vlastita tezina: " << tezina << " kg" << std::endl
+              << "Ukupna tezina: " << DajUkupnuTezinu() << " kg" << std::endl;
     }
 };
 
@@ -24,10 +30,17 @@ class Sanduk : public Spremnik {
     int broj_predmeta;
     double tezina_predmeta;
 public:
-    Sanduk(double tezina, const char sadrzaj[], int broj_predmeta, double tezina_predmeta) : Spremnik("Sanduk", tezina, sadrzaj), broj_predmeta(broj_predmeta), tezina_predmeta(tezina_predmeta) {}
+    Sanduk(double tezina, const char sadrzaj[], int broj_predmeta, double tezina_predmeta) : Spremnik("Sanduk", tezina,
+                                                                                                      sadrzaj),
+                                                                                             broj_predmeta(
+                                                                                                     broj_predmeta),
+                                                                                             tezina_predmeta(
+                                                                                                     tezina_predmeta) {}
+
     virtual double DajUkupnuTezinu() const override {
         return DajTezinu() + broj_predmeta * tezina_predmeta;
     }
+
     Spremnik *DajKopiju() const override { return new Sanduk(*this); } // moze bez virtual.. redundant
 };
 
@@ -35,10 +48,13 @@ class Bure : public Spremnik {
 private:
     double tezina_tecnosti;
 public:
-    Bure(double tezina, const char sadrzaj[], double tezina_tecnosti) : Spremnik("Bure", tezina, sadrzaj), tezina_tecnosti(tezina_tecnosti) {}
+    Bure(double tezina, const char sadrzaj[], double tezina_tecnosti) : Spremnik("Bure", tezina, sadrzaj),
+                                                                        tezina_tecnosti(tezina_tecnosti) {}
+
     virtual double DajUkupnuTezinu() const override {
         return DajTezinu() + tezina_tecnosti;
     }
+
     virtual Spremnik *DajKopiju() const override { return new Bure(*this); }
 };
 
@@ -48,23 +64,105 @@ private:
     int kapacitet;
     Spremnik **spremnici;
 public:
-    explicit Skladiste(int kapacitet) : broj_spremnika(0), kapacitet(kapacitet), spremnici(new Spremnik *[kapacitet]{}) {}
+    explicit Skladiste(int kapacitet) : broj_spremnika(0), kapacitet(kapacitet),
+                                        spremnici(new Spremnik *[kapacitet]{}) {}
+
     ~Skladiste();
+
     Skladiste(const Skladiste &s);
+
     Skladiste(Skladiste &&s);
+
     Skladiste &operator=(Skladiste s);
+
 //    Skladiste &operator=(Skladiste &&s);
     void DodajSanduk(double tezina, const char sadrzaj[], int broj_predmeta, double tezina_predmeta);
+
     void DodajBure(double tezina, const char sadrzaj[], double tezina_tecnosti);
+
     Spremnik &DajNajlaksi();
+
     const Spremnik &DajNajlaksi() const;
+
     Spremnik &DajNajtezi();
+
     const Spremnik &DajNajtezi() const;
+
     int BrojPreteskih(double granica) const;
+
     void IzlistajSkladiste(std::ostream &izlas = std::cout) const;
+
     void UcitajPodatke(const char ime[]);
 //    void Dealociraj();
 };
+
+// destructor
+Skladiste::~Skladiste() {
+    for (int i = 0; i < broj_spremnika; i++)
+        delete spremnici[i];
+    delete[] spremnici;
+}
+
+// copy constructor
+Skladiste::Skladiste(const Skladiste &s) : broj_spremnika(s.broj_spremnika), kapacitet(s.kapacitet),
+                                           spremnici(new Spremnik *[kapacitet]{}) {
+    try {
+        for (int i = 0; i < s.broj_spremnika; i++)
+            spremnici[i] = s.spremnici[i]->DajKopiju();
+    } catch (...) {
+        for (int i = 0; i < broj_spremnika; i++)
+            delete spremnici[i];
+        delete[] spremnici;
+        throw;
+    }
+}
+
+// copy constructor without DajKopiju()
+//Skladiste::Skladiste(const Skladiste &s) : broj_spremnika(s.broj_spremnika), kapacitet(s.kapacitet),
+//                                           spremnici(new Spremnik *[s.kapacitet]{}) {
+//    try {
+//        for (int i = 0; i < broj_spremnika; i++) {
+//            if (typeid(s.spremnici[i]) == typeid(Sanduk))
+//                spremnici[i] = new Sanduk(*static_cast<Sanduk *>(s.spremnici[i]));
+//            else
+//                spremnici[i] = new Bure(*static_cast<Bure *>(s.spremnici[i]));
+//        }
+//    } catch (...) {
+//        for (int i = 0; i < broj_spremnika; i++)
+//            delete spremnici[i];
+//        delete[] spremnici;
+//        throw;
+//    }
+//}
+
+Skladiste::Skladiste(Skladiste &&s) : broj_spremnika(s.broj_spremnika), kapacitet(s.kapacitet),
+                                      spremnici(new Spremnik *[s.kapacitet]{}) {
+    s.spremnici = nullptr;
+    s.broj_spremnika = 0;
+}
+
+Skladiste &Skladiste::operator=(Skladiste s) {
+//    if(this != &s) {
+    std::swap(broj_spremnika, s.broj_spremnika);
+    std::swap(kapacitet, s.kapacitet);
+    std::swap(spremnici, s.spremnici);
+//    }
+    return *this;
+}
+
+void Skladiste::DodajSanduk(double tezina, const char sadrzaj[], int broj_predmeta, double tezina_predmeta) {
+    if (broj_spremnika == kapacitet)
+        throw std::domain_error("Popunjeno skladiste");
+    spremnici[broj_spremnika++] = new Sanduk(tezina, sadrzaj, broj_predmeta, tezina_predmeta);
+}
+
+void Skladiste::DodajBure(double tezina, const char *sadrzaj, double tezina_tecnosti) {
+    if (broj_spremnika == kapacitet)
+        throw std::domain_error("Popunjeno skladiste");
+    spremnici[broj_spremnika++] = new Bure(tezina, sadrzaj, tezina_tecnosti);
+}
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
