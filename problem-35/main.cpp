682fa0ee99329bceb8352608f4b3cfde9962137c
sblekic <sblekic1@etf.unsa.ba>
2024-06-23 00:18:38 +0200

    interface for generic class Stek

--- problem-35/main.cpp
+++ problem-35/main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 
+template<typename T>
+class Stek {
+private:
+    int broj_elemenata;
+    int kapacitet;
+    T *elementi;
+public:
+    Stek() : broj_elemenata(0), kapacitet(10), elementi(new T[10]) {}
+
+    Stek(const Stek &s);
+
+    Stek(Stek &&s);
+
+    Stek &operator=(const Stek &s);
+
+    Stek &operator=(Stek &&s);
+
+    ~Stek() { delete[] elementi; }
+
+    void DodajNaVrh(const T &element);
+
+    int DajVelicinu() const { return broj_elemenata; }
+
+    bool DaLiJePrazan() const { return broj_elemenata == 0; }
+
+    T &DajVrh();
+
+    const T &DajVrh() const;
+
+    void SkiniSaVrha();
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
