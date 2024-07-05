eb4577ee7aeb335404a64337cdeef7bc0b72e8bf
sblekic <sblekic1@etf.unsa.ba>
2024-07-05 11:44:02 +0200

    implemented first constructor, destructor and dajDuzinu for class String

--- problem-44/main.cpp
+++ problem-44/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <cstring>
+
+
+class String {
+    char *niz_znakova;
+    int duzina;
+
+    void alociraj() { niz_znakova = new char[duzina + 1]{}; } // +1 za '\0'
+
+public:
+    String(const char niz[] = "") : duzina(strlen(niz)) {
+        alociraj();
+        strcpy(niz_znakova, niz);
+    }
+
+    ~String() { delete[] niz_znakova; }
+
+    int DajDuzinu() const { return duzina; }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    String s("Mrvim dok jedem");
+    std::cout << s.DajDuzinu() << std::endl;
     return 0;
 }
