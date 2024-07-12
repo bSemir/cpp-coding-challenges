dda1f60d3d11581f67ccec45bd61c7a1537fe040
sblekic <sblekic1@etf.unsa.ba>
2024-07-12 10:13:52 +0200

    finished task 47.

--- problem-47/main.cpp
+++ problem-47/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+bool jeLiSimetrican(const std::string &str) {
+    if (str.length() <= 1) return false;
+    int i = 0, j = str.length() - 1;
+    while (i < j) {
+        if (str.at(i) != str.at(j)) return false;
+        i++;
+        j--;
+    }
+    return true;
+}
+
+std::vector<std::string> dajPodstringove(const std::string &s) {
+    std::vector<std::string> v;
+    for (int i = 0; i < s.size(); i++) {
+        if (s.at(i) != ' ' && (i == 0 || s.at(i - 1) == ' ')) {
+            std::string rijec;
+            while (s[i] != ' ' && i < s.length()) rijec.push_back(s[i++]);
+            if (jeLiSimetrican(rijec)) v.push_back(rijec);
+        }
+    }
+    return v;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Unesite string: ";
+    std::string s;
+    std::getline(std::cin, s);
+    std::cout << "Simetricni podstringovi: " << std::endl;
+    auto result = dajPodstringove(s);
+    for (const auto &x: result) std::cout << x << std::endl;
     return 0;
 }
