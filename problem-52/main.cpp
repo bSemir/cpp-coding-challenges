354103a01ae3ff25ba250033c80c066c7b55def1
sblekic <sblekic1@etf.unsa.ba>
2024-07-15 23:44:07 +0200

    trying to make work fragmented allocation first

--- problem-52/main.cpp
+++ problem-52/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 enum class NacinAlokacije {
     Fragmentirano, Kontinualno
@@ -6,12 +7,32 @@ enum class NacinAlokacije {
 
 int **TablicaMnozenja(int n, NacinAlokacije alokacija) {
     if (n <= 0) return nullptr;
-
-    // TODO
-    return nullptr;
+    int **niz = nullptr;
+    niz = new int *[n];
+    for (int i = 1; i <= n; i++) niz[i] = new int[n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            niz[i][j] = i * j;
+        }
+    }
+    return niz;
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Unesite format tablice: ";
+    int n;
+    std::cin >> n;
+    int **mat = TablicaMnozenja(n, NacinAlokacije::Fragmentirano);
+    if (!mat) std::cout << "Neuspjesna alokacija!";
+    else {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                std::cout << std::setw(4) << mat[i][j];
+            }
+            std::cout << std::endl;
+        }
+        for (int i = 0; i < n; i++) delete[] mat[i];
+        delete[] mat;
+    }
     return 0;
 }
