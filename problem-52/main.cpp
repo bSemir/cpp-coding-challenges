8664ac6ddbde8230258e70ada290f1e5da304acf
sblekic <sblekic1@etf.unsa.ba>
2024-07-15 23:29:30 +0200

    p52's starter code

--- problem-52/main.cpp
+++ problem-52/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+enum class NacinAlokacije {
+    Fragmentirano, Kontinualno
+};
+
+int **TablicaMnozenja(int n, NacinAlokacije alokacija) {
+    if (n <= 0) return nullptr;
+
+    // TODO
+    return nullptr;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
