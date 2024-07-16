f78742ce68d1ddffd45c07c9a41888d13fca8ee0
sblekic <sblekic1@etf.unsa.ba>
2024-07-16 18:25:30 +0200

    completed problem 54

--- problem-54/main.cpp
+++ problem-54/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <stdexcept>
+#include <valarray>
 
 
-int suma_stepeni_cifara(std::vector<int> v, int i) {
-    //TODO
+int suma_stepeni_cifara(int n, int i) {
+    int s = 0;
+    while (n != 0) {
+        s += std::pow(std::abs(n % 10), i);
+        n /= 10;
+    }
+    return s;
 }
 
 int **f(std::vector<int> &v, int n) {
@@ -15,11 +21,17 @@ int **f(std::vector<int> &v, int n) {
     int m = v.size();
     tip **mat{};
     try {
-        mat = new tip *[n];
+        mat = new tip *[n]{}; // niz pokazivaca!
         mat[0] = new tip[m * n];
         for (int i = 0; i < n; i++) {
             if (i != 0) mat[i] = mat[i - 1] + m;
-            // TODO: copy and sort elements
+            std::copy(v.begin(), v.end(), mat[i]);
+            std::sort(mat[i], mat[i] + m, [=](auto x, auto y) {
+                int s1 = suma_stepeni_cifara(x, i);
+                int s2 = suma_stepeni_cifara(y, i);
+                if (s1 != s2) return s1 < s2;
+                return x < y;
+            });
         }
     } catch (...) {
         delete[] mat;
@@ -29,6 +41,21 @@ int **f(std::vector<int> &v, int n) {
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Unesite 5 cijelih brojeva: ";
+    int n = 3;
+    std::vector<int> v(5);
+    for (auto &el: v) std::cin >> el;
+
+    int **niz = f(v, n);
+    std::cout << "👇\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < v.size(); j++) {
+            std::cout << niz[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    delete[] niz[0];
+    delete[] niz;
+
     return 0;
 }
