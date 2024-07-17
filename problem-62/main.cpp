4e491084bf529dd16e08dc1c7c7c65bc2d047845
sblekic <sblekic1@etf.unsa.ba>
2024-07-18 00:02:34 +0200

    finished my own version of Fold - Umotaj

--- problem-62/main.cpp
+++ problem-62/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <set>
+
+
+template<typename TipElemenata, typename FunTip>
+TipElemenata Umotaj(const TipElemenata &kontejner, FunTip f) {
+    // uklanjamo 'const' i 'reference' iz TipElemenata
+    std::remove_cv_t<std::remove_reference_t<decltype(*kontejner.begin())>> v{};
+    // drugi nacin:
+    // using value_type = typename TipElemenata::value_type;
+    // value_type v{};
+
+    TipElemenata rezultat;
+
+    for (const auto &e: kontejner) {
+        v = f(v, e);
+        rezultat.insert(rezultat.end(), v);
+    }
+    return rezultat;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Unesite 4 elementa skupa: ";
+    std::set<int> skup;
+    for (int i = 0; i < 4; i++) {
+        int x;
+        std::cin >> x;
+        skup.insert(x);
+    }
+
+    std::set<int> novi_skup = Umotaj(skup, [](int x, int y) { return x * x + y * y; });
+
+    std::cout << "Novi skup: ";
+    for (int x: novi_skup) std::cout << x << " ";
+    std::cout << std::endl;
+
     return 0;
 }
