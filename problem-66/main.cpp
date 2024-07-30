53c9ef11339b9801874de18c4d004fff61a8dac7
sblekic <sblekic1@etf.unsa.ba>
2024-07-31 01:17:48 +0200

    made some slow progress on p66

--- problem-66/main.cpp
+++ problem-66/main.cpp
@@ -6,9 +6,17 @@
 
 template<typename tip>
 void aritmeticka_sredina(tip pocetak, tip iza_kraja) {
-    int vel = std::distance(pocetak, iza_kraja);
-//     std::cout << "size: " << vel << std::endl;
-    if (vel < 2) throw std::domain_error("Prekratak slijed");
+    // duzina
+    int size = std::distance(pocetak, iza_kraja);
+    if (size < 2) throw std::domain_error("Prekratak slijed");
+    // sredina
+    tip iter_sredina = std::next(pocetak, size / 2);
+    auto suma1 = *pocetak, suma2 = *iter_sredina;
+    // sum of first half
+    while(++pocetak != iter_sredina) suma1 += *pocetak;
+    // sum of second half
+    while(++iter_sredina != iza_kraja) suma2 += *iter_sredina;
+    // TODO: correctly divide and compare two sums
 }
 
 int main() {
@@ -20,3 +28,7 @@ int main() {
     }
     return 0;
 }
+
+// NOTE:
+// “advance” modificira proslijeđeni iterator da pokazuje na novo mjesto, a
+// “next” ne, nego samo vraća novi iterator koji pokazuje na novo mjesto kao rezultat
