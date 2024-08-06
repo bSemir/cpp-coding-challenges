13d8c8f6aa85030d1ece0f36ecf4f89febbba99e
sblekic <sblekic1@etf.unsa.ba>
2024-08-06 09:35:31 +0200

    added small anecdote on one edge case

--- problem-68/main.cpp
+++ problem-68/main.cpp
@@ -11,3 +11,7 @@ int main() {
 // Explanation:
 // m points to the 4th element of the array a, which is 4. n is a reference to *m, so n is 4.
 // After m += 3, m points to the 7th element of the array a, which is out of bounds. However, n is still 4, so the output is 4.
+
+// Addition:
+// If we change `m += 3` to `*m += 3`, the output will be 7. This is because we are incrementing the value at the memory location pointed by m by 3.
+// In this case it's 4 + 3 = 7.
