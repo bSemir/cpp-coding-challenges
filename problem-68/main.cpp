1d765b66e1a1d453c706720f5f50bf65f4a95f89
bSemir <semir.blekich@gmail.com>
2024-08-08 21:41:45 +0200

    added and explained new example

--- problem-68/main.cpp
+++ problem-68/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
+
 
 int main() {
     // 1
@@ -13,6 +16,14 @@ int main() {
     for (int k: b) k *= 2;
     for (int &k: b) k += 2;
     for (int k: b) std::cout << k << " ";
+
+    std::cout << std::endl;
+
+    // 3
+    std::string str{"a ba ab aba bab"};
+    std::cout << str << " ";
+    std::remove_copy(str.rbegin(), str.rend(), std::back_inserter(str), ' ');
+    std::cout << str;
     return 0;
 }
 
@@ -32,3 +43,10 @@ int main() {
 // The first range-based for loop does not modify the elements of the array b, because k is a copy of the elements.
 // The second range-based for loop modifies the elements of the array b by adding 2 to each element. This is because `k` is a reference to the elements.
 // The third range-based for loop prints the modified elements of the array `b`.
+
+// ### 3 ###
+// Output: a ba ab aba bab a ba ab aba babbabababa
+// Explanation:
+// The string `str` is "a ba ab aba bab". First cout prints the original string.
+// The `remove_copy` functions copies the elements of the string in reverse order to the end of the string, removing spaces.
+// Here, reverse iterators are used to iterate over the string in reverse order. The output string becomes "a ba ab aba bab babbabababa".
