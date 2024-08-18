a85d25e7fa9a1c0bd0aa83def18cf6eb4aa25611
bSemir <semir.blekich@gmail.com>
2024-08-18 09:57:15 +0200

    v1 of function 'IzbaciSuvisneRazmake' that removes unnecessary spaces from a sentence

---
+++ problem-71/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+std::string IzbaciSuvisneRazmake(std::string &s) {
+    std::string result;
+    for (int i = 0; i < s.length(); i++) {
+        if (s.at(i) != ' ') {
+            std::string word;
+            while (s.at(i) != ' ') word.push_back(s[i++]);
+            result = result + word + ' ';
+        }
+    }
+    return result;
+}
+
+int main() {
+    std::cout << "Unesite recenicu: "; //   "   Ovo    je primjer.    "
+    std::string sentence;
+    std::getline(std::cin, sentence);
+    std::string result = IzbaciSuvisneRazmake(sentence);
+    std::cout << "👉" << result << std::endl;
+    return 0;
+}
