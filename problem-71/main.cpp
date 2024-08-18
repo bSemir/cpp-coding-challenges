7cfd437f231e8e2bcdc8ace5c48c4dd3d771b7a3
bSemir <semir.blekich@gmail.com>
2024-08-18 10:19:15 +0200

    started implementing better exception safety for p71

--- problem-71/main.cpp
+++ problem-71/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <stdexcept>
+
+bool DaLiSadrziSlova(const std::string &s) {
+    return std::count_if(s.begin(), s.end(), [](char c) { return std::isalpha(c); }) > 3;
+}
 
 std::string IzbaciSuvisneRazmake(std::string &s) {
     if (s.length() == 0 || s.length() < 4)
         throw std::domain_error("Neispravan string!");
+    if (!DaLiSadrziSlova(s)) throw "Neispravna recenica!";
     std::string result;
     for (int i = 0; i < s.length(); i++) {
         if (s.at(i) != ' ') {
@@ -15,7 +22,7 @@ std::string IzbaciSuvisneRazmake(std::string &s) {
     return result;
 }
 
-// TODO: remove spaces at the end if found, try-catch..
+// TODO: remove spaces at the end if found, implement better algorithm to check if sentence is valid
 
 int main() {
     try {
