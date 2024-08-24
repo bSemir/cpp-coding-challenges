ea635b23145fc7ce91f18f8b7d32e98575f3b564
bSemir <semir.blekich@gmail.com>
2024-08-24 17:41:06 +0200

    implemented simple and the most efficient algorithm to find greatest common divisor

--- problem-74/main.cpp
+++ problem-74/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+int NZD(const std::vector<int> &v) {
+    if (v.empty()) return -1;
+    int najmanji = *std::min_element(v.begin(), v.end());
+    for (int i = najmanji; i > 0; i--) {
+        bool djeljiv = true;
+        for (int j = 0; j < v.size(); j++) {
+            if (v[j] % i != 0) {djeljiv = false;
+            break;
+            }
+        }
+        if (djeljiv) return i;
+    }
+    return 1;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> v{305, 5630, 210, 8470, 7305};
+    if (NZD(v) != -1) std::cout << "NZD: " << NZD(v) << std::endl;
+    else std::cout << "Greska ili je rezultat 1\n";
     return 0;
 }
