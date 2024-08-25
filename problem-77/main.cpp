96040bc239e79ab91a0fd1b9d5f9372a1f65db13
bSemir <semir.blekich@gmail.com>
2024-08-26 01:02:09 +0200

    wrote some comments regarding other solution to p77

--- problem-77/main.cpp
+++ problem-77/main.cpp
@@ -21,3 +21,12 @@ int main() {
     for (int i = 0; i < v.size(); i++) std::cout << v[i] << " ";
     return 0;
 }
+
+// NOTE:
+// We could also do it like this where we loop backwards to avoid indexing problems (when we insert negated number
+// behind current number, original elements don't shift before we come to them since we are looping from behind):
+// int originalSize = v.size();
+// for(int i = originalSize - 1; i >= 0; i--) {
+//     int suprotni = -v.at(i);
+//       v.insert(v.begin() + i + 1, suprotni);
+// }
