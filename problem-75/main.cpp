371a23520fd1b691e59288190a32cb5ebe0f6879
bSemir <semir.blekich@gmail.com>
2024-08-25 16:31:42 +0200

    implemented p75 - KopirajBezDuplikata

--- problem-75/main.cpp
+++ problem-75/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <string>
+
+template<typename T1, typename T2>
+T1 KopirajBezDuplikata(T1 pocetak, T1 iza_kraja, T2 odrediste) {
+    for (auto p = pocetak; p != iza_kraja; p++) {
+        bool unique = true;
+        // da li je vidjen ranije
+        for (auto q = pocetak; q != p; q++) {
+            if (*q == *p) {
+                unique = false;
+                break;
+            }
+        }
+        if (unique)
+            *odrediste++ = *p;
+    }
+    return odrediste;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Unesite string: ";
+    std::string s;
+    std::getline(std::cin, s);
+    std::string result(s.size(), ' '); // dest str must not be empty
+    auto kraj_novog = KopirajBezDuplikata(s.begin(), s.end(), result.begin());
+    result.resize(kraj_novog - result.begin());
+    std::cout << "String bez duplikata: " << result << std::endl;
     return 0;
 }
