deab9e9e1d64969608a32776c878be109a382d81
bSemir <semir.blekich@gmail.com>
2024-08-27 13:46:36 +0200

    implemented continual type of dynamic memory allocation in problem-78

--- problem-78/main.cpp
+++ problem-78/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <stdexcept>
 
-int **KreirajTrougao(int n) {
+// Fragmentirana alokacija
+int **KreirajTrougaoFragmentirano(int n) {
     if (n <= 0) throw std::domain_error("Broj redova mora biti pozitivan");
 
     int **matrica = nullptr;
@@ -25,20 +26,56 @@ int **KreirajTrougao(int n) {
     return matrica;
 }
 
+// Kontinualna alokacija
+int **KreirajTrougaoKontinualno(int n) {
+    if (n <= 0) throw std::domain_error("Broj redova mora biti pozitivan");
+
+    int **matrica{};
+    try {
+        matrica = new int *[n]{}; // niz od `n` pokazivaca
+        matrica[0] = new int[n * n]; // prvi pokazivac pokazuje na niz velicine `n*n` koji je dovoljan
+        for (int i = 1; i < n; i++)
+            matrica[i] = matrica[i - 1] + 2 * i + 1; // postavljanje pokazivaca na pocetke redova
+        // popunjavanje
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++)
+                matrica[i][j] = i + 1 - j;
+            for (int k = i; k <= 2 * i + 1; k++)
+                matrica[i][k] = k + 1 - i;
+        }
+    } catch (...) {
+        delete[] matrica;
+        throw;
+    }
+    return matrica;
+}
+
 int main() {
     std::cout << "Koliko zelite redova: ";
     int n;
     std::cin >> n;
+//    try {
+//        auto mat = KreirajTrougaoFragmentirano(n);
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < 2 * i + 1; j++) {
+//                std::cout << mat[i][j] << " ";
+//            }
+//            std::cout << "\n";
+//        }
+//        for (int i = 0; i < n; i++) delete mat[i];
+//        delete[] mat;
+//    } catch (std::exception &e) {
+//        std::cout << "Izuzetak: " << e.what() << std::endl;
+//    }
     try {
-        auto mat = KreirajTrougao(n);
+        auto mat = KreirajTrougaoKontinualno(n);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < 2 * i + 1; j++) {
                 std::cout << mat[i][j] << " ";
             }
             std::cout << "\n";
         }
-        for (int i = 0; i < n; i++) delete mat[i];
-        delete[] mat;
+        delete[] mat[0]; delete[] mat;
     } catch (std::exception &e) {
         std::cout << "Izuzetak: " << e.what() << std::endl;
     }
