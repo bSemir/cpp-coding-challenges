2168d672b7ade01abaa35f35f87b6877c6a75bde
bSemir <semir.blekich@gmail.com>
2024-08-28 11:35:51 +0200

    really simple check for number of rows and columns (will be improved)

--- problem-80/main.cpp
+++ problem-80/main.cpp
@@ -3,7 +3,6 @@
 
 template<typename tip>
 void KreirajMatricu(tip **&A, int br_redova, int br_kolona, bool nacin = true, tip vrijednost = {}) {
-    if (br_redova <= 0 || br_kolona <= 0) throw;
     A = nullptr;
     try {
         if (nacin) {
@@ -42,6 +41,10 @@ int main() {
     std::cout << "Unesite broj redova i kolona matrice: ";
     int br_redova, br_kolona;
     std::cin >> br_redova >> br_kolona;
+    if (br_redova <= 0 || br_kolona <= 0) {
+        std::cout << "Unijeli ste pogresne dimenzije redova i/ili kolona!";
+        return 0;
+    }
     std::cout << "Unesite nacin alokacije(1 - Kontinualno, 2 - Fragmentirano): ";
     int x;
     std::cin >> x;
