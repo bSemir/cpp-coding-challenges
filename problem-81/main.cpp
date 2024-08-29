594e1ec3b61b0b46b1ae406da7f89f118ec44724
bSemir <semir.blekich@gmail.com>
2024-08-29 13:23:27 +0200

    tested p81 in main

--- problem-81/main.cpp
+++ problem-81/main.cpp
@@ -2,22 +2,12 @@
 #include <vector>
 #include <stdexcept>
 
-// kontinualna
 
 template<typename IterTip1, typename IterTip2, typename FunTip>
 auto GeneraliziraniSpoljasnjiProizvod(IterTip1 &a, IterTip2 &b, FunTip f) {
     // tip
-    using T = std::remove_reference_t<decltype(f(*std::begin(a), std::begin(b)))>;
-    // broj redova
-//    int br_redova_a = 0;
-//    for (auto p = std::begin(a); p != std::end(a); p++) br_redova_a++;
-//    int br_redova_b = 0;
-//    for (auto p = std::begin(b); p != std::end(b); p++) br_redova_b++;
-//    int br_redova;
-//    if (br_redova_a > br_redova_b)
-//        br_redova = br_redova_a;
-//    else br_redova = br_redova_b;
-//
+    using T = std::remove_reference_t<decltype(f(*std::begin(a), *std::begin(b)))>;
+
     int m = 0; // a
     for (auto p = std::begin(a); p != std::end(a); p++) m++;
     int n = 0; // b
@@ -25,14 +15,14 @@ auto GeneraliziraniSpoljasnjiProizvod(IterTip1 &a, IterTip2 &b, FunTip f) {
     T **C = nullptr;
 
     try {
-        C = new T *[m];
+        C = new T *[m]; // alociramo niz od m pokazivaca na tip T jer je matrica dimenzija m x n
         C[0] = new T[m * n];
         for (int i = 1; i < m; i++) C[i] = C[i - 1] + n;
     } catch (...) {
         delete[] C;
         throw std::range_error("Nema dovoljno memorije");
     }
-    auto it1 = std::begin(a);
+    auto it1 = std::begin(a); // it1 pokazuje na prvi element sekvence a
     for (int i = 0; i < m; i++) {
         auto it2 = std::begin(b);
         for (int j = 0; j < n; j++) {
@@ -44,6 +34,35 @@ auto GeneraliziraniSpoljasnjiProizvod(IterTip1 &a, IterTip2 &b, FunTip f) {
 }
 
 int main() {
-    
+    std::cout << "Unesite duzinu prve sekvence: ";
+    int m;
+    std::cin >> m;
+    std::cout << "Elementi prve sekvence: ";
+    std::vector<int> a(m);
+    for (auto &el: a) std::cin >> el;
+
+    std::cout << "Unesite duzinu druge sekvence: ";
+    int n;
+    std::cin >> n;
+    std::cout << "Elementi druge sekvence: ";
+    std::vector<int> b(n);
+    for (auto &el: b) std::cin >> el;
+
+    std::cout << "Generalizirani spoljasnji proizvod uz f(x,y)=x+2y: \n";
+    try {
+        auto C = GeneraliziraniSpoljasnjiProizvod(a, b, [=](int x, int y) {
+            return x + 2 * y;
+        });
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                std::cout << C[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+        delete[] C[0];
+        delete[] C;
+    } catch (std::exception &e) {
+        std::cout << "Izuzetak: " << e.what() << std::endl;
+    }
     return 0;
 }
