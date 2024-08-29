7eb1786371c1a8b3ec9d845031ca426eb5286f45
bSemir <semir.blekich@gmail.com>
2024-08-29 22:06:44 +0200

    completed p82 - 'SlikeUOgledalu' function that accepts vector of ints and then returns new vec but with reversed elements

--- problem-82/main.cpp
+++ problem-82/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <vector>
+
+int izvrni_cifre(int n) {
+    if (n == 0) return 0;
+    bool negativan = n < 0;
+    std::string s = "";
+    while (n != 0) {
+        auto c = std::to_string(std::abs(n % 10));
+        std::string str{c};
+        s += str;
+        n /= 10;
+    }
+    int x = std::stoi(s);
+    if (negativan)
+        return -x;
+    return x;
+}
+
+std::vector<int> SlikeUOgledalu(const std::vector<int> &v) {
+    std::vector<int> vec;
+    for (int i = 0; i < v.size(); i++) {
+        int izvrnuti = izvrni_cifre(v.at(i));
+        vec.push_back(izvrnuti); // vec.push_back(izvrni_cifre(v.at(i)));
+    }
+    return vec;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> v;
+    std::cout << "Unesite element po element (0 za kraj): \n";
+    int n;
+    do {
+        std::cout << "Element: ";
+        std::cin >> n;
+        if (n == 0) break;
+        v.push_back(n);
+    } while (n != 0);
+    auto vec = SlikeUOgledalu(v);
+    for (int i = 0; i < v.size(); i++)
+        std::cout << "Slika u ogledalu broja " << v[i] << " glasi " << vec[i] << std::endl;
     return 0;
 }
