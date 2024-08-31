652f3eaaf76949332aa77a6b01bd4749c75e7013
bSemir <semir.blekich@gmail.com>
2024-08-31 16:29:00 +0200

    some functional changes in p86

--- problem-86/main.cpp
+++ problem-86/main.cpp
@@ -2,11 +2,9 @@
 #include <string>
 
 void dodaj_n_razmaka(std::string &s, int n) {
-    for (int i = 0; i < s.size(); i++) {
-        if (s.at(i) != ' ') {
-            for (int j = 0; j < n; j++)
-                s.insert(s.begin() + i + j + 1, ' ');
-        }
+    for (int i = 0; i < s.size(); i += n + 1) {
+        for (int j = 0; j < n; j++)
+            s.insert(s.begin() + i + j + 1, ' ');
     }
 }
 
