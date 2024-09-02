e32d53deaf839770fde27aa131494d992f61282c
bSemir <semir.blekich@gmail.com>
2024-09-02 14:16:02 +0200

    finished a task p88 (if curr vec element is product of 5 consecutive natural numbers, push true else push false

--- problem-88/main.cpp
+++ problem-88/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <vector>
+
+bool proizvod_5_uzastopnih(int x) {
+    if (x < 120) return false; // najmanji moguci proizvod: 1*2*3*4*5 = 120
+    for (int i = 1; i <= x; i++) {
+        int p = i * (i + 1) * (i + 2) * (i + 3) * (i + 4);
+        if (p == x) return true;
+        if (p > x) break;
+    }
+    return false;
+}
+
+std::vector<bool> f(const std::vector<int> &v) {
+    std::vector<bool> res;
+    for (int e: v) res.push_back(proizvod_5_uzastopnih(e));
+    return res;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> v{720, 110}; // true false
+    auto res = f(v);
+    for (int i = 0; i < res.size(); i++) std::cout << std::boolalpha << res.at(i) << " ";
     return 0;
 }
