f809e6a8cc3551849f4a62c54706cbfbbc24acce
bSemir <semir.blekich@gmail.com>
2024-09-15 23:39:14 +0200

    modified descr and wrote some boilerplate code in main.cpp

---
+++ problem-91/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> permute(std::vector<int> &nums) {
+        // I do not know how to solve this problem rn
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
