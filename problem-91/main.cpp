39a3bcc6128bf6420f83e7fcb5900a1d8736e46e
bSemir <semir.blekich@gmail.com>
2024-09-17 12:35:00 +0200

    solution for medium leetcode problem - Number of Laser Beams in a Bank

--- problem-91/main.cpp
+++ problem-91/main.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 class Solution {
 public:
-    std::vector<std::vector<int>> permute(std::vector<int> &nums) {
-        // I do not know how to solve this problem rn
+    int numberOfBeams(std::vector<std::string> &bank) {
+        std::vector<int> counters(bank.size());
+        int total = 0;
+
+        for (int i = 0; i < bank.size(); i++) {
+            bool hasLasers = false;
+            int lasersInCurrentRow = 0;
+            for (int j = 0; j < bank[i].size(); j++) {
+                if (bank[i][j] == '1') {
+                    lasersInCurrentRow++;
+                    hasLasers = true;
+                }
+            }
+            if (hasLasers) {
+                counters.push_back(lasersInCurrentRow);
+            }
+        }
+
+        for (int k = 0; k < counters.size() - 1; k++) {
+            // [3, 2, 1]
+            total += counters[k] * counters[k + 1];
+        }
+
+        return total;
     }
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Solution solution;
+    std::vector<std::string> bank = {"000", "111", "000"};
+    std::cout << solution.numberOfBeams(bank) << std::endl;
     return 0;
 }
